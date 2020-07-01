22a8bc29c8f52cad5cf481d945da69ec062baaf6
Emutisya <mutisya.elizabeth@strathmore.edu>
2020-07-01 13:33:08 +0300

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,47 +15,6 @@
               <form method="post"  action="{{ route('register') }}" enctype="multipart/form-data">
                 @csrf
 
-<<<<<<< HEAD
-                <div class="card-body register-card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-
-
-                        <div class="form-group row">
-                            <label for="Hospital" class="col-md-4 col-form-label text-md-right">{{ __('Hospital') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="hospital" type="text" class="form-control @error('hospital') is-invalid @enderror" name="hospital" value="{{ old('hospital') }}" required autocomplete="hospital" autofocus>
-
-                                @error('hospital')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-=======
               <div class="form-label-group">
                   <!--<input type="text" name="name" id="inputPassword" class="form-control" placeholder="Full Name" required>
                   <label for="inputPassword">Full Name</label>-->
@@ -68,7 +27,6 @@
                   <label for="Hospital" >{{ __('Hospital') }} </label>
 
                 </div>
->>>>>>> 475c11997881c58233c7b64e72a3431b5227032b
 
                 <div class="form-label-group">
                   <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
